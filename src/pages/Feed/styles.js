011e4645c5b374da54da3a7272c215a6cbbbc1c8
ricassiocosta <riksiocosta@gmail.com>
2020-04-06 18:43:00 -0300

    The feed can be scrolled; Content still static

--- src/pages/Feed/styles.js
+++ src/pages/Feed/styles.js
@@ -1,8 +1,32 @@
 import styled from 'styled-components'
 
 export const FeedPage = styled.div`
-  font-family: 'Arvo';
+  font-family: 'Roboto';
   background: #E5E5E5;
   width: 100%;
   height: 100vh;
 `
+export const Content = styled.div`
+  width: 800px;
+  display: flex;
+  margin: 0 auto;
+  margin-top: 10px
+`
+export const Sidebar = styled.div`
+  width: 250px;
+  height: 290px;
+  border-radius: 10px;
+  background: #fff;
+`
+export const FeedHistory = styled.div`
+  width: 530px;
+  height: 92vh;
+  border-radius: 10px;
+  background: #fff;
+  margin-left: 26px;
+  overflow-y: auto;
+  ::-webkit-scrollbar{
+    display: none
+  }
+`
+
