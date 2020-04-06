abb2fa2ffbd70bd648b4940bf1fb05d00a49d711
Lucas Gomes <lucasdeoliveira887@gmail.com>
2020-04-06 15:38:41 -0300

    Added a services folder to add the methods to call api

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,7 @@
-const axios = require('axios')
-const { PROTOCOL, API_URL } = require('../../env')
+import axios from 'axios'
+import { PROTOCOL, API_URL } from '../env'
 
-module.exports = {
-  async authenticate (githubToken) {
-    const response = await axios.post(`${PROTOCOL}://${API_URL}/auth`, { github_token: githubToken });
-    return response.data.token
-  }
+export const authenticate = async (githubToken) => {
+  const response = await axios.post(`${PROTOCOL}://${API_URL}/auth`, { github_token: githubToken });
+  return response.data.token
 }
--- src/services/github.js
+++ src/services/github.js
@@ -1,9 +1,7 @@
-const axios = require('axios')
-const { PROTOCOL, API_URL } = require('../../env')
+import axios from 'axios'
+import { PROTOCOL, API_URL } from '../env'
 
-module.exports = {
-  async getGithubToken (code) {
-    const response = await axios.get(`${PROTOCOL}://${API_URL}/callback/github`, { params: { code } });
-    return response.data.token
-  }
+export const getGithubToken = async (code) => {
+  const response = await axios.get(`${PROTOCOL}://${API_URL}/callback/github`, { params: { code } });
+  return response.data.token
 }
