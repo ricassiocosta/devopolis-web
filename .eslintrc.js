d5f434319fc656c9e8e5e4600154f89c9547db09
Lucas Gomes <lucasdeoliveira887@gmail.com>
2020-04-13 22:37:05 -0300

    Added more rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'plugin:react/recommended',
+    'plugin:prettier/recommended',
     'standard'
   ],
   globals: {
@@ -19,8 +20,47 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: [
-    'react'
+    'react',
+    'prettier'
   ],
   rules: {
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-await-in-loop': 'error',
+    'quote-props':0,
+    'dot-notation': 'warn',
+    'prefer-const': 'warn',
+    'no-return-await': 'error',
+    'require-await': 'error',
+    'class-methods-use-this': 'warn',
+    'consistent-return': 'warn',
+    'max-len': ['warn', {
+      'code': 100,
+      'ignoreStrings': true,
+      'ignoreRegExpLiterals': true,
+      'ignoreTrailingComments': true,
+      'ignoreComments': true,
+      "ignoreTemplateLiterals": true
+    }],
+    eqeqeq: 'error',
+    'prefer-destructuring': [
+      'warn',
+      {
+        VariableDeclarator: {
+          array: true,
+          object: true
+        },
+        AssignmentExpression: {
+          array: false,
+          object: false
+        }
+      }
+    ],
+    'object-curly-spacing': ['error', 'always'],
+    'no-console': process.env.NODE_ENV === "production" ? 2 : 0,
+    'quotes': ['error', 'single'],
+    'curly': ['error', 'all'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
   }
 }
